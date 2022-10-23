fc0c891a8eb36c9aff79e9e780a31980516997ca
Niannu Gao <niannu.gao@student.manchester.ac.uk>
2022-10-23 22:04:01 +0100

    fidoreaNPC test fix some error

--- tests/games/stendhal/server/maps/ados/city/MakeupArtistNPCTest.java
+++ tests/games/stendhal/server/maps/ados/city/MakeupArtistNPCTest.java
@@ -1,7 +1,6 @@
 package games.stendhal.server.maps.ados.city;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static utilities.SpeakerNPCTestHelper.getReply;
 
@@ -9,8 +8,6 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import games.stendhal.server.core.engine.SingletonRepository;
-import games.stendhal.server.entity.npc.ConversationPhrases;
 import games.stendhal.server.entity.npc.SpeakerNPC;
 import games.stendhal.server.entity.npc.fsm.Engine;
 import games.stendhal.server.entity.player.Player;
@@ -21,14 +18,11 @@ import utilities.ZonePlayerAndNPCTestImpl;
 
 public class MakeupArtistNPCTest extends ZonePlayerAndNPCTestImpl {
 	
-	private static final String ZONE_NAME = "testzone";
+	private static final String ZONE_NAME = "int_ados_city";
 	
 	private Player player;
 	private SpeakerNPC fidoreaNpc;
 	private Engine fidoreaEngine;
-	
-	private static final String HELP_REPLY = "I sell masks. If you don't like your mask, you can #return and I will remove it, or you can just wait %s, until it wears off.";
-	private static final String BUY_REPLY = "To buy a mask will cost 20. Do you want to buy it?";
 	
 	
 	@BeforeClass
@@ -44,76 +38,67 @@ public class MakeupArtistNPCTest extends ZonePlayerAndNPCTestImpl {
 		super.setUp();
 
 		player = createPlayer("player");
-		fidoreaNpc = SingletonRepository.getNPCList().get("Fidorea");
+		fidoreaNpc = getNPC("Fidorea");
 		fidoreaEngine = fidoreaNpc.getEngine();
 	}
 	
 	public MakeupArtistNPCTest() {
 		super(ZONE_NAME, "Fidorea");
+		addZoneConfigurator(new MakeupArtistNPC(), ZONE_NAME);
 	}
 	
 	@Test
-	public void testDialogue() {
+	public void testHiandBye() {
 		startConversation();
-
-		checkReply("job", "I am a makeup artist.");
-		checkReply("help", HELP_REPLY);
 
 		endConversation();
 	}
 
 	private void startConversation() {
-		fidoreaEngine.step(player, ConversationPhrases.GREETING_MESSAGES.get(0));
-		assertTrue(fidoreaNpc.isTalking());
+		assertTrue(fidoreaEngine.step(player, "hi"));
 		assertEquals("Hi, there. Do you need #help with anything?", getReply(fidoreaNpc));
 	}
 	
 	private void endConversation() {
-		fidoreaEngine.step(player, ConversationPhrases.GOODBYE_MESSAGES.get(0));
-		assertFalse(fidoreaNpc.isTalking());
+		assertTrue((fidoreaEngine.step(player, "bye")));
 		assertEquals("Bye, come back soon.", getReply(fidoreaNpc));
 	}
 
-	private void checkReply(String question, String expectedReply) {
-		fidoreaEngine.step(player, question);
-		assertTrue(fidoreaNpc.isTalking());
-		assertEquals(expectedReply, getReply(fidoreaNpc));
-	}
 	
 	@Test
 	public void checkBuyMask() {
 		assertTrue(fidoreaEngine.step(player, "hi"));
 		assertEquals("Hi, there. Do you need #help with anything?", getReply(fidoreaNpc));
 		
-		assertTrue(equipWithMoney(player, 120));
+		assertTrue(equipWithMoney(player, 500));
 		
 		assertTrue(fidoreaEngine.step(player, "buy dog mask"));
-		assertEquals(BUY_REPLY, getReply(fidoreaNpc));
+		assertEquals("To buy a dog mask will cost 20. Do you want to buy it?", getReply(fidoreaNpc));
 		assertTrue(fidoreaEngine.step(player, "yes"));
 		assertEquals((Integer) 998, player.getOutfit().getLayer(("mask")));
 		
 		assertTrue(fidoreaEngine.step(player, "buy fox mask"));
-		assertEquals(BUY_REPLY, getReply(fidoreaNpc));
+		assertEquals("To buy a fox mask will cost 20. Do you want to buy it?", getReply(fidoreaNpc));
 		assertTrue(fidoreaEngine.step(player, "yes"));
 		assertEquals((Integer) 999, player.getOutfit().getLayer(("mask")));
 		
 		assertTrue(fidoreaEngine.step(player, "buy frog mask"));
-		assertEquals(BUY_REPLY, getReply(fidoreaNpc));
+		assertEquals("To buy a frog mask will cost 20. Do you want to buy it?", getReply(fidoreaNpc));
 		assertTrue(fidoreaEngine.step(player, "yes"));
 		assertEquals((Integer) 995, player.getOutfit().getLayer(("mask")));
 		
 		assertTrue(fidoreaEngine.step(player, "buy monkey mask"));
-		assertEquals(BUY_REPLY, getReply(fidoreaNpc));
+		assertEquals("To buy a monkey mask will cost 20. Do you want to buy it?", getReply(fidoreaNpc));
 		assertTrue(fidoreaEngine.step(player, "yes"));
 		assertEquals((Integer) 997, player.getOutfit().getLayer(("mask")));
 		
 		assertTrue(fidoreaEngine.step(player, "buy penguin mask"));
-		assertEquals(BUY_REPLY, getReply(fidoreaNpc));
+		assertEquals("To buy a penguin mask will cost 20. Do you want to buy it?", getReply(fidoreaNpc));
 		assertTrue(fidoreaEngine.step(player, "yes"));
 		assertEquals((Integer) 996, player.getOutfit().getLayer(("mask")));
 		
 		assertTrue(fidoreaEngine.step(player, "buy bear mask"));
-		assertEquals(BUY_REPLY, getReply(fidoreaNpc));
+		assertEquals("To buy a bear mask will cost 20. Do you want to buy it?", getReply(fidoreaNpc));
 		assertTrue(fidoreaEngine.step(player, "yes"));
 		assertEquals((Integer) 994, player.getOutfit().getLayer(("mask")));
 		
@@ -123,3 +108,4 @@ public class MakeupArtistNPCTest extends ZonePlayerAndNPCTestImpl {
 	
 	
 }
+
